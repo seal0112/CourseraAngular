2ce3a1299984e01572f7f023b3d17245553672ef
seal0112 <seal0112@gmail.com>
2017-11-22 20:47:16 +0800

    Single Page Application Part2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -12,7 +12,7 @@ export class DishService {
   }
 
   getDish(id: number): Dish{
-  	return DISHES.filter((dish) => {dish.id === id})[0];
+  	return DISHES.filter((dish) => dish.id === id)[0];
   }
 
   getFeaturedDish(): Dish{
