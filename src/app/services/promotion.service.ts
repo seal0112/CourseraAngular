d0fe63dea960defc811cb69e677ffb1777f0c889
seal0112 <seal0112@gmail.com>
2017-11-28 18:31:16 +0800

    Assignment 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion'; 
-import { PROMOTIONS} from '../shared/promotions';
+import { PROMOTIONS } from '../shared/promotions';
 
 @Injectable()
 export class PromotionService {
