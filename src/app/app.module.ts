093f5922258bb19b3c37d4b5b1c73b7404c7212f
seal0112 <seal0112@gmail.com>
2017-11-29 18:11:42 +0800

    Reactive Forms Part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -41,7 +42,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
         LoginComponent
