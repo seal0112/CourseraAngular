02897ddd58fee8266082b8460e0140e895f1f99e
seal0112 <seal0112@gmail.com>
2017-11-30 18:15:38 +0800

    Reactive Forms Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,7 +12,7 @@ export class ContactComponent implements OnInit {
 
 	feedbackForm: FormGroup;
 	feedback: Feedback;
-	contacttype = ContactType;
+	contactType = ContactType;
 
   constructor(private fb: FormBuilder) {
   	this.createForm();
@@ -22,20 +22,28 @@ export class ContactComponent implements OnInit {
   }
 
   createForm() {
-  	this.feedbackForm = this.fb.group({
-  		firstname: '',
-  		lastname: '',
-  		telnum: 0,
-  		email: '',
-  		agree: false,
-  		contacttype: 'None',
-  		message: '',
-  	});
+    this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
   }
 
   onSubmit() {
-  	this.feedback = this.feedbackForm.value;
-  	console.log(this.feedback);
-  	this.feedbackForm.reset();
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
+    	firstname: '',
+    	lastname: '',
+    	telnum: '',
+    	email: '',
+    	agree: false,
+    	contacttype: 'None',
+    	message: ''
+    });
   }
 }
